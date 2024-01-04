be70a804b9a9a3f4c86e0249471fbc6aa2dcc52f
wexenvagyok <120109536+wexenvagyok@users.noreply.github.com>
2024-01-04 12:11:56 +0100

    0074 - Szekvenciális tevékenység

---
+++ feladat74.py
@@ -0,0 +1,13 @@
+print("Farkas Balázs","\n","2023.01.04.","\n")
+
+felsoErtek = 27
+szorzoErtek = 3
+Hozzaadando = 2
+elsoAlsoErtek = 23
+masodikAlsoErtek = 55
+
+felsoEredmeny = felsoErtek * szorzoErtek + Hozzaadando
+alsoEredmeny = elsoAlsoErtek + masodikAlsoErtek
+Eredmeny = felsoEredmeny / alsoEredmeny
+
+print("Az eredmény: "+ str(Eredmeny))
