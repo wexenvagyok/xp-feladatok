160701136b319608f0156d2a6f1c022e2624d743
wexenvagyok <120109536+wexenvagyok@users.noreply.github.com>
2024-01-04 10:07:33 +0100

    0006 - Szekvenciális tevékenység

---
+++ feladat6.py
@@ -0,0 +1,11 @@
+print("Farkas Balázs","\n","2023.01.04.","\n")
+
+ismetles = 7*"#"
+szamok = ["2", "3", "7", "8", "9"]
+print(ismetles)
+print("#"+szamok[1]+"#"+szamok[2]+"#"+szamok[3]+"#")
+print(ismetles) 
+print("#"+szamok[4]+"#"+szamok[3]+"#"+szamok[0]+"#")
+print(ismetles) 
+print("#"+szamok[3]+"#"+szamok[0]+"#"+szamok[1]+"#")
+print(ismetles)
