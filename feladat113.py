0b1bf60db0b7154856a449c914ec0a9dda632ea3
wexenvagyok <120109536+wexenvagyok@users.noreply.github.com>
2024-01-08 22:23:08 +0100

    0113 - Szekvenciális tevékenység

---
+++ feladat113.py
@@ -0,0 +1,13 @@
+print("Termény\t\tDűlő\t\tTermés (q)")
+print("-"*39)
+print("búza\t\tTönkös\t\t370\nrepce\t\tBóvár\t\t249\nrozs\t\tHontoka\t\t317\nárpa\t\tBükkös\t\t415")
+
+
+#Termény      Dűlő        Termés (q)
+#---------------------------------------
+#búza         Tönkös      370
+#repce        Bóvár       249
+#rozs         Hontoka     317
+#árpa         Bükkös      415
+
+#Ügyeljen arra, hogy az oszlopok között minimum egy tabulátorral tagolt üres hely legyen.
