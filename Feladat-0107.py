e637362fb4dc3ac63f468437ea72fed6b7743ee1
wexenvagyok <120109536+wexenvagyok@users.noreply.github.com>
2024-01-08 20:54:41 +0100

    0107 - Szekvenciális tevékenység

---
+++ Feladat-0107.py
@@ -0,0 +1,13 @@
+print("Szerző: Farkas Balázs\n")
+print("1\tFekete Gábor\tSzeged\n2\tPéti Feren\tSzolnok\n3\tLápos Géza\tSzeged\n4\tGender Lajos\tSzeged\n5\tGeszti Árpád\tSzolnok\n6\tLantos Zoltán\tMiskolc\n7\tFehér Irén\tDebrecen\n8\tProg Lajos\tSzolnok")
+
+
+
+#1        Fekete Gábor     Szeged
+#2        Péti Feren       Szolnok
+#3        Lápos Géza       Debrecen
+#4        Gender Lajos     Szeged
+#5        Geszti Árpád     Szolnok
+#6        Lantos Zoltán    Miskolc
+#7        Fehér Irén       Debrecen
+#8        Prog Lajos       Szolnok
