62ece521217ea1953f79116d6ddeadc17407181e
wexenvagyok <120109536+wexenvagyok@users.noreply.github.com>
2024-01-09 09:43:41 +0100

    0409- Szekvenciális tevékenység

---
+++ feladat409.py
@@ -0,0 +1,9 @@
+#Olvassunk be egy valós számot, és döntsük el, hogy szigorúan 10 és 20 közé esik-e!ű
+
+szam = eval(input("Valós szám ellenőrző\nAdd meg a számot: "))
+
+if szam <= 20 and  szam >= 10:
+    print("A szám tíz és húsz között van.")
+else:
+    print("A szám nincs tíz és húsz között.")
+    
