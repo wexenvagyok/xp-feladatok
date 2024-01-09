06cbee1d93f2b86ee4e2126a4705b13dd69d67c6
wexenvagyok <120109536+wexenvagyok@users.noreply.github.com>
2024-01-09 22:41:29 +0100

    0109 - Szekvenciális tevékenység

---
+++ feladat109.py
@@ -0,0 +1,5 @@
+#select * from ped where user="fantom";
+#select * from ped where user="gabor" or user="lali";
+
+print("Szerző:\tFarkas Balázs\n")
+print("select * from ped where user=\"fantom\";\nselect * from ped where user=\"gabor\" or user=\"lali\";")
