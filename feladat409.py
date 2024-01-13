afb5d7ec4b080f993aa5894b8b224cadb0c145d8
wexenvagyok <120109536+wexenvagyok@users.noreply.github.com>
2024-01-13 09:48:41 +0100

    0403 - Szelekciós tevékenység

--- feladat409.py
+++ feladat409.py
@@ -1,6 +1,6 @@
 #Olvassunk be egy valós számot, és döntsük el, hogy szigorúan 10 és 20 közé esik-e!ű
 
-szam = eval(input("Valós szám ellenőrző\nAdd meg a számot: "))
+szam = float(input("Valós szám ellenőrző\nAdd meg a számot: "))
 
 if szam <= 20 and  szam >= 10:
     print("A szám tíz és húsz között van.")
