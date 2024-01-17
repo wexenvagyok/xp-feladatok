d2c7eb31be73f2ca458b5df24a890403f322d96e
wexenvagyok <120109536+wexenvagyok@users.noreply.github.com>
2024-01-17 08:27:20 +0100

    0502 - Iterációs tevékenység

--- feladat502.py
+++ feladat502.py
@@ -1,7 +1,7 @@
 #Kérjen be egy számot. Ha 50-nél kisebb kérje újra.
 
 while True:
-    szam = szam = float(input("Adj meg egy számot: "))
+    szam = float(input("Adj meg egy számot: "))
     if szam >= 50:
         break
     else:
